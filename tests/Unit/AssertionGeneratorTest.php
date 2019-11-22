903032944efdc520f696dd69527fefd6aaa8b8d8
Jon Cram <jon@webignition.net>
2019-11-22 16:44:47 +0000

    Create generator

---
+++ tests/Unit/AssertionGeneratorTest.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace webignition\BasilAssertionGenerator\Tests\Unit;
+
+use webignition\BasilAssertionGenerator\AssertionGenerator;
+use webignition\BasilModel\Assertion\AssertionComparison;
+use webignition\BasilModel\Assertion\AssertionInterface;
+use webignition\BasilModel\Assertion\ExaminationAssertion;
+use webignition\BasilModel\Identifier\DomIdentifier;
+use webignition\BasilModel\Value\DomIdentifierValue;
+
+class AssertionGeneratorTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @dataProvider generateDataProvider
+     */
+    public function testGenerate(string $assertionString, AssertionInterface $expectedAssertion)
+    {
+        $assertionGenerator = AssertionGenerator::createGenerator();
+
+        $this->assertEquals($expectedAssertion, $assertionGenerator->generate($assertionString));
+    }
+
+    public function generateDataProvider(): array
+    {
+        return [
+            'default' => [
+                'assertionString' => '".selector" exists',
+                'expectedAssertion' => new ExaminationAssertion(
+                    '".selector" exists',
+                    new DomIdentifierValue(
+                        new DomIdentifier('.selector')
+                    ),
+                    AssertionComparison::EXISTS
+                ),
+            ],
+        ];
+    }
+}
